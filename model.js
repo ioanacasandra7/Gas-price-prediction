47f2f27e76e77afcd502e000e8978616ea940994
ryann-green <65695678+ryann-green@users.noreply.github.com>
2020-11-14 14:08:42 -0600

    working on converting model to js

--- model.js
+++ model.js
@@ -1,5 +1,32 @@
-console.log('hi')
+const url = `http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPMRR_PTE_YORD_DPG.W`
 
-tf.loadLayersModel("Notebooks/jsmodel/model.json").then(model => {
+tf.loadLayersModel("jsmodel/model.json").then(model => {
     this._model = model;
+
+    let array = [1[]
 })
+
+// d3.json(url).then( function(d){
+//     // read in data from the API
+//     let gasPrices = d.series[0].data
+
+//     // Difference data from the API
+//     let arr =[]
+//     for(let i =1; i<11; i++){
+//         arr.push(gasPrices[gasPrices.length -i][1] - gasPrices[gasPrices.length -i-1][1])
+//     }
+//     arr = arr.reverse()
+    
+//     // Load model
+//     tf.loadLayersModel("Notebooks/jsmodel/model.json").then(model => {
+//         // convert data into a tensor
+//         data = tf.tensor(arr, [1,10,1])
+
+//         // run the model
+//         var prediction = model.predict(data).as1D();
+
+//         // print prediction to console
+//         prediction.array().then(a => console.log(parseFloat(a)));
+//     });
+// });
+    

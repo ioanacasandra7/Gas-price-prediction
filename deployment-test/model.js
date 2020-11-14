4a1ba6d601c51be7fc9f92cd5d30dca6e1e4bdc5
Mark Piraino <m.piraino3@gmail.com>
2020-11-14 06:57:18 -0600

    making first model

--- deployment-test/model.js
+++ deployment-test/model.js
@@ -1,16 +1,25 @@
 const url = `http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPMRR_PTE_YORD_DPG.W`
 d3.json(url).then( function(d){
+    // read in data from the API
     let gasPrices = d.series[0].data
+
+    // Difference data from the API
     let arr =[]
     for(let i =1; i<11; i++){
         arr.push(gasPrices[gasPrices.length -i][1] - gasPrices[gasPrices.length -i-1][1])
     }
     arr = arr.reverse()
-    console.log(arr)
-        tf.loadLayersModel("jsmodel/model.json").then(model => {
-            data = tf.tensor(arr, [1,10,1])
-            var prediction = model.predict(data).as1D();
-            prediction.array().then(a => console.log(parseFloat(a)));
-        });
+    
+    // Load model
+    tf.loadLayersModel("jsmodel/model.json").then(model => {
+        // convert data into a tensor
+        data = tf.tensor(arr, [1,10,1])
+
+        // run the model
+        var prediction = model.predict(data).as1D();
+
+        // print prediction to console
+        prediction.array().then(a => console.log(parseFloat(a)));
     });
+});
     

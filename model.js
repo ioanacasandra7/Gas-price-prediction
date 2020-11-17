4e3a1bb98c025f1dd61d8d6cdb39f4b687bf1458
ryann-green <65695678+ryann-green@users.noreply.github.com>
2020-11-16 19:01:55 -0600

    completed conversion of model over to JS. Needed to adjust script

--- model.js
+++ model.js
@@ -1,32 +1,32 @@
-const url = `http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPMRR_PTE_YORD_DPG.W`
+east_url = `http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPM0_PTE_R10_DPG.W`
+mid_url=`http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPM0_PTE_R20_DPG.W`
+gulf_url=`http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPM0_PTE_R30_DPG.W`
+rock_url=`http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPM0_PTE_R40_DPG.W`
+west_url=`http://api.eia.gov/series/?api_key=${API_KEY}&series_id=PET.EMM_EPM0_PTE_R50_DPG.W`
 
-tf.loadLayersModel("jsmodel/model.json").then(model => {
-    this._model = model;
+d3.json(east_url).then( function(d){
+    // read in data from the API
+    let gasPrices = d.series[0].data
 
-    let array = [1[]
-})
+    // Difference data from the API
+    let arr =[]
+    for(let i =1; i<131; i++){
+        arr.push(gasPrices[gasPrices.length -i][1] - gasPrices[gasPrices.length -i-1][1])
+    }
+    arr = arr.reverse()
 
-// d3.json(url).then( function(d){
-//     // read in data from the API
-//     let gasPrices = d.series[0].data
-
-//     // Difference data from the API
-//     let arr =[]
-//     for(let i =1; i<11; i++){
-//         arr.push(gasPrices[gasPrices.length -i][1] - gasPrices[gasPrices.length -i-1][1])
-//     }
-//     arr = arr.reverse()
+    console.log(arr)
     
-//     // Load model
-//     tf.loadLayersModel("Notebooks/jsmodel/model.json").then(model => {
-//         // convert data into a tensor
-//         data = tf.tensor(arr, [1,10,1])
+    // Load model
+    tf.loadLayersModel("models/model.json").then(model => {
+        // convert data into a tensor
+        data = tf.tensor(arr, [1,10,13])
 
-//         // run the model
-//         var prediction = model.predict(data).as1D();
+        // run the model
+        var prediction = model.predict(data).as1D();
 
-//         // print prediction to console
-//         prediction.array().then(a => console.log(parseFloat(a)));
-//     });
-// });
+        // print prediction to console
+        prediction.array().then(a => console.log(parseFloat(a)));
+    });
+});
     

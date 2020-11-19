7e9a51bdb231d820f0a24e5c04bf16c59311b97b
Mark Piraino <m.piraino3@gmail.com>
2020-11-19 12:55:22 -0600

    adding other charts

--- static/js/runModelMP.js
+++ static/js/runModelMP.js
@@ -84,7 +84,7 @@ function processData(d,modelChoice) {
                 petroleum_importProcessed[i]])
             }  
         }
-        console.log(week)
+        
     return week
 }
 
@@ -235,7 +235,7 @@ function makeModelChart(data, timeStamps, region, timesteps, modelChoice) {
         }else if(region == "midwest"){
             regionName = "Midwest"
         }else if(region == "rockyMountain"){
-            regionName = "Rocky Mountains"
+            regionName = "Rocky Mountain"
         }
         
 
@@ -250,7 +250,317 @@ function makeModelChart(data, timeStamps, region, timesteps, modelChoice) {
     });
 };
 
+function makeGasPriceChart(data, timeStamps, region) {
+        pastData = []
+
+        for (let i = 0; i < 96; i++) {
+            pastData.push({
+                time: timeStamps[i],
+                eastCoast: parseFloat(data[0][i]),
+                midwest: parseFloat(data[1][i]),
+                gulfCoast: parseFloat(data[2][i]),
+                rockyMountain: parseFloat(data[3][i]),
+                westCoast: parseFloat(data[4][i]),
+                WTI : parseFloat(data[5][i]),
+                import : parseFloat(data[6][i]),
+                export : parseFloat(data[7][i])
+            })}
+
+
+        chartWidth = parseInt(d3.select("#chart1").style("width"));
+        chartHeight = parseInt(d3.select("#chart1").style("height"));
+
+        // set the dimensions and margins of the graph
+        let margin = { top: 10, right: 30, bottom: 30, left: 60 },
+            width = chartWidth - margin.left - margin.right,
+            height = chartHeight - margin.top - margin.bottom;
+
+        // append the svg object to the body of the page
+        let svgGasPrice = d3.select("#chart1")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+
+        xMin = Math.min(d3.min(pastData, function (d) { return d.time; }), d3.min(pastData, function (d) { return d.time; }))
+        xMax = Math.max(d3.max(pastData, function (d) { return d.time; }), d3.max(pastData, function (d) { return d.time; }))
+
+        // Add X axis --> it is a date format
+        var xScale = d3.scaleTime()
+            .domain([xMin, xMax])
+            .range([0, width]);
+
+        svgGasPrice.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale));
+
+        yMin = Math.min(d3.min(pastData, function (d) { return d[region]; }), d3.min(pastData, function (d) { return d[region]; })) - 0.25
+        yMax = Math.max(d3.max(pastData, function (d) { return d[region]; }), d3.max(pastData, function (d) { return d[region]; })) + 0.25
+
+        // Add Y axis
+        var yScale = d3.scaleLinear()
+            .domain([yMin, yMax])
+            .range([height, 0]);
+
+        svgGasPrice.append("g")
+            .call(d3.axisLeft(yScale));
+
+        //create line
+        let makeLineGasPrice = d3.line()
+            .x(d => xScale(d.time))
+            .y(d => yScale(d[region]));
+
+        // Add the past line
+        svgGasPrice.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", makeLineGasPrice(pastData))
+
+        //add y label
+        svgGasPrice.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Retail Price of Gas");
+
+        let regionName = ""
+
+        if(region == "westCoast"){
+            regionName = "West Coast"
+        } else if(region == "eastCoast"){
+            regionName = "East Coast"
+        }else if(region == "gulfCoast"){
+            regionName = "Gulf Coast"
+        }else if(region == "midwest"){
+            regionName = "Midwest"
+        }else if(region == "rockyMountain"){
+            regionName = "Rocky Mountain"
+        }
+        
+
+        //add Chart Title
+        svgGasPrice.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 0 - (margin.top / 2 - 20))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("text-decoration", "underline")
+            .text(regionName);
+    
+};
+
+function makeWTIChart(data, timeStamps) {
+    pastData = []
+
+    for (let i = 0; i < 96; i++) {
+        pastData.push({
+            time: timeStamps[i],
+            eastCoast: parseFloat(data[0][i]),
+            midwest: parseFloat(data[1][i]),
+            gulfCoast: parseFloat(data[2][i]),
+            rockyMountain: parseFloat(data[3][i]),
+            westCoast: parseFloat(data[4][i]),
+            WTI : parseFloat(data[5][i]),
+            import : parseFloat(data[6][i]),
+            export : parseFloat(data[7][i])
+        })}
+
+
+    chartWidth = parseInt(d3.select("#chart2").style("width"));
+    chartHeight = parseInt(d3.select("#chart2").style("height"));
+
+    // set the dimensions and margins of the graph
+    let margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = chartWidth - margin.left - margin.right,
+        height = chartHeight - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    let svgWTI = d3.select("#chart2")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    xMin = Math.min(d3.min(pastData, function (d) { return d.time; }), d3.min(pastData, function (d) { return d.time; }))
+    xMax = Math.max(d3.max(pastData, function (d) { return d.time; }), d3.max(pastData, function (d) { return d.time; }))
+
+    // Add X axis --> it is a date format
+    var xScale = d3.scaleTime()
+        .domain([xMin, xMax])
+        .range([0, width]);
+
+    svgWTI.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    yMin = Math.min(d3.min(pastData, function (d) { return d.WTI; }), d3.min(pastData, function (d) { return d.WTI; })) - 10
+    yMax = Math.max(d3.max(pastData, function (d) { return d.WTI; }), d3.max(pastData, function (d) { return d.WTI; })) + 20
+
+    // Add Y axis
+    var yScale = d3.scaleLinear()
+        .domain([yMin, yMax])
+        .range([height, 0]);
+
+    svgWTI.append("g")
+        .call(d3.axisLeft(yScale));
+
+    //create line
+    let makeLineWTI = d3.line()
+        .x(d => xScale(d.time))
+        .y(d => yScale(d.WTI));
+
+    // Add the past line
+    svgWTI.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", makeLineWTI(pastData))
+
+    //add y label
+    svgWTI.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Spot Price of WTI");
+    
+    //add Chart Title
+    svgWTI.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2 - 20))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("WTI Crude");
+
+};
+
+function makeImportExportChart(data, timeStamps) {
+    pastData = []
+
+    for (let i = 0; i < 96; i++) {
+        pastData.push({
+            time: timeStamps[i],
+            eastCoast: parseFloat(data[0][i]),
+            midwest: parseFloat(data[1][i]),
+            gulfCoast: parseFloat(data[2][i]),
+            rockyMountain: parseFloat(data[3][i]),
+            westCoast: parseFloat(data[4][i]),
+            WTI : parseFloat(data[5][i]),
+            import : parseFloat(data[6][i]),
+            export : parseFloat(data[7][i])
+        })}
+
+
+    chartWidth = parseInt(d3.select("#chart3").style("width"));
+    chartHeight = parseInt(d3.select("#chart3").style("height"));
+
+    // set the dimensions and margins of the graph
+    let margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = chartWidth - margin.left - margin.right,
+        height = chartHeight - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    let svgImportExport = d3.select("#chart3")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    xMin = Math.min(d3.min(pastData, function (d) { return d.time; }), d3.min(pastData, function (d) { return d.time; }))
+    xMax = Math.max(d3.max(pastData, function (d) { return d.time; }), d3.max(pastData, function (d) { return d.time; }))
+
+    // Add X axis --> it is a date format
+    var xScale = d3.scaleTime()
+        .domain([xMin, xMax])
+        .range([0, width]);
+
+    svgImportExport.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    yMin = Math.min(d3.min(pastData, function (d) { return d.import; }), d3.min(pastData, function (d) { return d.export; })) - 2000
+    yMax = Math.max(d3.max(pastData, function (d) { return d.import; }), d3.max(pastData, function (d) { return d.export; })) + 2000
+
+    // Add Y axis
+    var yScale = d3.scaleLinear()
+        .domain([yMin, yMax])
+        .range([height, 0]);
+
+    svgImportExport.append("g")
+        .call(d3.axisLeft(yScale));
+
+    //create line
+    let makeLineImport = d3.line()
+        .x(d => xScale(d.time))
+        .y(d => yScale(d.import));
+
+    let makeLineExport = d3.line()
+        .x(d => xScale(d.time))
+        .y(d => yScale(d.export));
+
+    // Add the past line
+    svgImportExport.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", makeLineImport(pastData))
+
+    svgImportExport.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "green")
+        .attr("stroke-width", 1.5)
+        .attr("d", makeLineExport(pastData))
+
+    //add y label
+    svgImportExport.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Thousands of Barrels");
+    
+    //add Chart Title
+    svgImportExport.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2 - 20))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("Import/Export");
+
+    //add legend
+    svgImportExport.append("circle").attr("cx",210).attr("cy",230).attr("r", 3).style("fill", "steelblue")
+    svgImportExport.append("circle").attr("cx",210).attr("cy",250).attr("r", 3).style("fill", "green")
+    svgImportExport.append("text").attr("x", 220).attr("y", 230).text("Imports").style("font-size", "10px").attr("alignment-baseline","middle")
+    svgImportExport.append("text").attr("x", 220).attr("y", 250).text("Exports").style("font-size", "10px").attr("alignment-baseline","middle")
+
+};
+
  function makePage(region, timesteps, modelChoice){
+     //add model descriptions
+    let modelDescription = d3.select("#model-description")
+    if(modelChoice == "all_channel_linear_js"){
+        modelDescription.html("<pre><code> model = keras.models.Sequential([keras.Input(shape=(X_train.shape[1],X_train.shape[2])),keras.layers.Flatten(),keras.layers.Dense(units=8)]) </code></pre>")
+    }else if (modelChoice == "all_channel_LSTM_js"){
+        modelDescription.html("<pre><code>model = keras.models.Sequential([keras.layers.LSTM(units = 8, input_shape=(X_train.shape[1],X_train.shape[2]), return_sequences=False),keras.layers.Dense(units=8)])</code></pre>")
+    }else if(modelChoice == "LSTM_model"){
+        modelDescription.html("<pre><code>model = tf.keras.models.Sequential([tf.keras.layers.LSTM(32, return_sequences=True),tf.keras.layers.Dense(units=1)])</code></pre>")
+    }
 
     Promise.all([
         d3.json(eastCoastURL),
@@ -279,7 +589,10 @@ function makeModelChart(data, timeStamps, region, timesteps, modelChoice) {
             timeStamps.unshift(d3.timeWeek.offset(timeStamps[0], 1))
         }
 
-        makeModelChart(data, timeStamps, region, timesteps, modelChoice)
+        makeModelChart(data, timeStamps, region, timesteps, modelChoice);
+        makeGasPriceChart(data, timeStamps, region);
+        makeWTIChart(data, timeStamps);
+        makeImportExportChart(data, timeStamps);
 
     });}
 
@@ -293,8 +606,12 @@ dropDownRegion.on("change", onSelect);
 // event handler function for drop down menu
 function onSelect(){
     d3.select("#model-chart").html("")
+    d3.select("#chart1").html("")
+    d3.select("#chart2").html("")
+    d3.select("#chart3").html("")
     let modelChoice = d3.select("#model-selector").property("value");
     let region = d3.select("#region-selector").property("value");
+    
     makePage(region, 12, modelChoice);
 }
 
